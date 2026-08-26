7fb460928a02a481d0b7a3596d7922d3f863c5b6
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-08-26 13:12:21 +0300

    create reservation service

---
+++ src/services/reservationService.js
@@ -0,0 +1,46 @@
+import api from './api'
+
+async function createReservation(formData) {
+  const response = await api.post('/reservations', formData)
+  return response.data
+}
+
+async function getMyReservations() {
+  const response = await api.get('/reservations/my-reservations')
+  return response.data
+}
+
+async function getPharmacyReservations() {
+  const response = await api.get('/reservations/pharmacy')
+  return response.data
+}
+
+async function updateReservationStatus(id, status) {
+  const response = await api.patch(`/reservations/${id}/status`, {
+    status
+  })
+
+  return response.data
+}
+
+async function cancelReservation(id) {
+  const response = await api.delete(`/reservations/${id}`)
+  return response.data
+}
+
+async function uploadPrescription(id, imageUrl) {
+  const response = await api.post(`/reservations/${id}/prescription`, {
+    imageUrl
+  })
+
+  return response.data
+}
+
+export {
+  createReservation,
+  getMyReservations,
+  getPharmacyReservations,
+  updateReservationStatus,
+  cancelReservation,
+  uploadPrescription
+}
