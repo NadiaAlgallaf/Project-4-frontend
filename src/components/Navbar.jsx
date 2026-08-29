0ca52f589fc7bd744f36f9192e802ddf749f0035
Nadia Algallaf <nadiaalgallaf164@gmail.com>
2026-08-29 12:07:47 +0300

    update navbar with role based navigation for users and pharmacies

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,50 @@
 import { Link } from 'react-router'
 import { useAuth } from '../context/AuthContext'
-import { useTranslation } from "react-i18next";
-import LanguageSwitcher from "./LanguageSwitcher";
+import { useTranslation } from 'react-i18next'
+import LanguageSwitcher from './LanguageSwitcher'
 
 function Navbar() {
-  const { t } = useTranslation();
+  const { t } = useTranslation()
+  const { logout, user } = useAuth()
 
-  const { logout, user} = useAuth()
   return (
     <nav>
-      <Link to='/'>{t('nav.home')}</Link>
-      {user 
-      ? 
-      (<>
-      <button onClick={logout}>{t('nav.signOut')}</button>
-      </>) : 
-      (<>
-        <Link to='/sign-up'>{t('nav.signUp')}</Link>
-        <Link to='/sign-in'>{t('nav.signIn')}</Link>
-      </>)}
-    <LanguageSwitcher />
+      {/* Links available for everyone */}
+      <Link to="/">{t('nav.home')}</Link>
+      <Link to="/medicines">Medicines</Link>
+      <Link to="/pharmacies">Pharmacies</Link>
 
+      {user ? (
+        <>
+          {/* Links for User */}
+          {user.role === 'User' && (
+            <Link to="/my-reservations">My Reservations</Link>
+          )}
+
+          {/* Links for Pharmacy */}
+          {user.role === 'Pharmacy' && (
+            <>
+              <Link to="/pharmacy/dashboard">Dashboard</Link>
+
+              <Link to="/pharmacy/inventory">Inventory</Link>
+
+              <Link to="/pharmacy/reservations">Reservations</Link>
+            </>
+          )}
+
+          {/* Available for any signed in*/}
+          <button onClick={logout}>{t('nav.signOut')}</button>
+        </>
+      ) : (
+        <>
+          {/* Links for users who are not signed in */}
+          <Link to="/sign-up">{t('nav.signUp')}</Link>
+
+          <Link to="/sign-in">{t('nav.signIn')}</Link>
+        </>
+      )}
+
+      <LanguageSwitcher />
     </nav>
   )
 }
